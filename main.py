2760381ab2da51ae1ec4a8137e3254aab17e7691
Peerachet Porkaew <peerachet.porkaew@gmail.com>
2021-12-01 07:33:52 +0700

    classifier

--- main.py
+++ main.py
@@ -16,6 +16,33 @@ def get_children_list(node):
         out.append(ch.value)
     return " ".join(out)
 
+def get_leaves(node, output):
+    if len(node.children) > 0:
+        for ch in node.children:
+            get_leaves(ch,output)
+    else:
+        output.append(node.value)
+
+
+def get_all_leaves(node):
+    output = []
+    get_leaves(node,output)
+    return output
+
+def find_children(node,value):
+    if node.value == value:
+        return node
+    else:
+        found = None
+        for ch in node.children:
+            found = find_children(ch,value)
+            if found is not None:
+                return found 
+        
+        return None
+
+
+
 def apply_reordering(node):
 
     if len(node.children) == 0:
@@ -31,6 +58,15 @@ def apply_reordering(node):
             if children == "DNP NP": # ..的 NP --> NP ..的
                 #print("FOUND")
                 node.children = [node.children[1], node.children[0]]
+            
+            if children == "QP NP": # QP (两 只) NP (狗)
+                node.children = [node.children[1], node.children[0]]
+                noun = get_all_leaves(node.children[0])[-1]
+                print(noun)
+                node = find_children(node.children[1],"M")
+                cl = node.children[0].value
+                node.children[0].value = f"$CL({cl},{noun})"
+
    
     elif node.value == "DNP":  # DNP (NP (..) DEG (的) )
         if len(node.children) == 2:
@@ -43,17 +79,7 @@ def apply_reordering(node):
 
     return
 
-
-def get_leaves(node, output):
-    if len(node.children) > 0:
-        for ch in node.children:
-            get_leaves(ch,output)
-    else:
-        output.append(node.value)
-
-
-
-out = parse_chinese("父親的母親")
+out = parse_chinese("我有两百只狗。")
 print(out)
 y = get_penn_tree_from_string(out)
 print(y[0].children[0].value)
@@ -63,6 +89,3 @@ apply_reordering(y[0].children[0])
 output = []
 get_leaves(y[0].children[0],output)
 print(output)
-
-
-
